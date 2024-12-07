65ba2765245c8b8f7fb173f8d36825d4a3f6c70b
royanrosyad85 <royan.rosyad21@mhs.uinjkt.ac.id>
2024-12-07 15:03:56 +0700

    fix database name

--- src/services/getAllData.js
+++ src/services/getAllData.js
@@ -1,7 +1,10 @@
 const { Firestore } = require('@google-cloud/firestore');
 
 async function getAllData() {
-    const db = new Firestore();
+    const db = new Firestore({
+        projectId: 'submissionmlgc-royanrosyad',
+        databaseId: 'prediksi-cancer'
+    });
     const predictCollection = db.collection('predictions');
     
     const allData = await predictCollection.get();
