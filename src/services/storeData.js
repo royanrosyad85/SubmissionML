9362a607f45ea0e8d9ba8fe457b49d4962f85fb2
royanrosyad85 <royan.rosyad21@mhs.uinjkt.ac.id>
2024-12-07 14:56:59 +0700

    update ne firestore database

--- src/services/storeData.js
+++ src/services/storeData.js
@@ -1,10 +1,20 @@
 const { Firestore } = require('@google-cloud/firestore');
- 
+
 async function storeData(id, data) {
-  const db = new Firestore();
- 
-  const predictCollection = db.collection('prediction');
-  return predictCollection.doc(id).set(data);
+  const db = new Firestore({
+    projectId: 'submissionmlgc-royanrosyad',
+    databaseId: 'prediksi-cancer'
+  });
+
+  const predictCollection = db.collection('predictions');
+
+  try {
+    await predictCollection.doc(id).set(data);
+    console.log('Document successfully written!');
+  } catch (error) {
+    console.error('Error writing document:', error);
+    throw error;
+  }
 }
- 
+
 module.exports = storeData;
